c11a24970c19b92cbfba077c109b293c960889cc
YoussefGomaa <youssef123gomaa@gmail.com>
2023-03-06 01:39:55 +0200

    Implement Decrypt

--- SecurityPackage/securitylibrary/MainAlgorithms/HillCipher.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/HillCipher.cs
@@ -9,47 +9,145 @@ namespace SecurityLibrary
     /// <summary>
     /// The List<int> is row based. Which means that the key is given in row based manner.
     /// </summary>
-    public class HillCipher :  ICryptographicTechnique<List<int>, List<int>>
+    public class HillCipher : ICryptographicTechnique<List<int>, List<int>>
     {
-        public List<int> list(List<int> plainText, List<int> cipherText)
-        {
-            List<int> newlist = new List<int>();
-            bool check = true;
-            for (int i = 0; i < 26; i++)
-            {
-                for (int j = 0; j < 26; j++)
-                {
-                    check = true;
-                    for (int k = 0; k < plainText.Count - 1; k++)
-                    {
-                        int x;
-                        x = (i * plainText[k]) + (j * plainText[k + 1]);
-                        x %= 26;
-                        if (x != cipherText[k])
-                        {
-                            check = false;
-                            break;
-                        }
-                    }
-                    if (check)
-                    {
-                        newlist.Add(i);
-                        newlist.Add(j);
-                        return newlist;
-                    }
-                }
-            }
-            return newlist;
-        }
+
         public List<int> Analyse(List<int> plainText, List<int> cipherText)
         {
             throw new InvalidAnlysisException();
         }
+        public int CalcDet(List<int> cell, bool check)
+        {
+            int det;
+            if (cell.Count == 4)
+                det = (cell[0] * cell[3]) - (cell[1] * cell[2]);
+            else
+            {
+                det = cell[0] * (cell[4] * cell[8] - cell[5] * cell[7]) -
+                      cell[1] * (cell[3] * cell[8] - cell[5] * cell[6]) +
+                      cell[2] * (cell[3] * cell[7] - cell[4] * cell[6]);
+            }
+            if (!check)
+            {
+                return det;
+            }
+            while (det < 0)
+                det += 26;
+
+            return det % 26;
+        }
+
+        public int CalB(int det)
+        {
+            for (int i = 1; i < 26; ++i)
+            {
+                if (i * det % 26 == 1)
+                    return i;
+            }
+            return -1;
+        }
+
+        public bool Check(List<int> key)
+        {
+            for (int i = 0; i < key.Count; ++i)
+            {
+                if (key[i] >= 0 || key[i] <= 25)
+                    return true;
+            }
 
 
+            return false;
+        }
+
+
+        public bool CheckDet(int det)
+        {
+            int a = 26, b = det;
+            while (b != 0)
+            {
+                int temp = a % b;
+                a = b;
+                b = temp;
+
+            }
+
+            return a == 1;
+        }
+
+        public int CalcMod(int val)
+        {
+            while (val < 0)
+            {
+                val += 26;
+            }
+            return val % 26;
+        }
         public List<int> Decrypt(List<int> cipherText, List<int> key)
         {
-            throw new NotImplementedException();
+            int m = (int)Math.Sqrt(key.Count);
+            int det = CalcDet(key, true);
+            int b = CalB(det);
+            bool GCD = CheckDet(det);
+            bool valid = Check(key);
+            if (!GCD || !valid)
+                throw new NotImplementedException();
+
+            List<int> list = new List<int>();
+            if (key.Count == 4)
+            {
+                int inverse = (1 / CalcDet(key, false));
+                list.Add(CalcMod(inverse * key[3]));
+                list.Add(CalcMod(key[1] * inverse * -1));
+                list.Add(CalcMod(key[2] * inverse * -1));
+                list.Add(CalcMod(inverse * key[0]));
+
+
+                return Encrypt(cipherText, list);
+            }
+            else
+            {
+                for (int i = 0; i < key.Count; ++i)
+                {
+                    list.Add(0);
+                }
+                List<int> L = new List<int>();
+
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        L.Clear();
+                        for (int k = 0; k < m; k++)
+                        {
+                            if (k == i)
+                                continue;
+                            for (int a = 0; a < m; a++)
+                            {
+                                if (a == j)
+                                    continue;
+                                L.Add(key[k * m + a]);
+                            }
+                        }
+                        list[i * m + j] = (b * (int)Math.Pow(-1, i + j) * CalcDet(L, true)) % 26;
+                        while (list[i * m + j] < 0)
+                        {
+                            list[i * m + j] += 26;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        key[j * m + i] = (int)list[i * m + j];
+                    }
+                }
+
+
+                return Encrypt(cipherText, key);
+            }
+
         }
 
 
