ff0f79d1824f922cbea9cdeea1d275b606fff96e
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2023-03-07 11:37:53 +0200

    Implement Analyse of Caesar

--- SecurityPackage/securitylibrary/MainAlgorithms/Ceaser.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/Ceaser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,15 @@ namespace SecurityLibrary
             key %= 26;
              for (int i = 0; i < plainText.Length; i ++)
              {
-                cipher += (char)((plainText[i] + key - 'a') % 26 + 'a');
+                if (char.IsUpper(plainText[i]))
+                {
+                    cipher += (char)((plainText[i] + key - 'A') % 26 + 'A');
+                }else
+                {
+                    cipher += (char)((plainText[i] + key - 'a') % 26 + 'a');
+                }
 
-            }
-            Console.WriteLine(cipher);
+             }
             return cipher;
         }
 
@@ -28,15 +34,30 @@ namespace SecurityLibrary
 
             for (int i = 0; i < cipherText.Length; i++)
             {
-                plainText += (char)((cipherText[i] - key - 'A' + 26) % 26 + 'A');
+                if (char.IsUpper(cipherText[i]))
+                {
+                    plainText += (char)((cipherText[i] - key - 'A' + 26) % 26 + 'A');
+
+                }else
+                {
+                    plainText += (char)((cipherText[i] - key - 'a' + 26) % 26 + 'a');
+                }
             }
-            Console.WriteLine(plainText);
             return plainText;
         }
 
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            // it doesn't matter if the origin text were upperCase or lowerCase, we just need to the shift value in either.
+            plainText = plainText.ToUpper();
+            cipherText = cipherText.ToUpper();
+            int diff = (int) plainText[0] - cipherText[0];
+            if (diff > 0) {
+                return 26 - diff;
+            }else
+            {
+                return Math.Abs(diff);
+            }       
         }
     }
 }
