2f846e05d2dc4e71dc98be97620efe4b63ee0793
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2023-03-08 11:51:35 +0200

    Implement Encrypt and Decrypt of RepeatKeyVigenere

--- SecurityPackage/securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -15,12 +15,61 @@ namespace SecurityLibrary
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            char[,] tabulaRecta = generateTabulaRecta();
+            string plainText = "";
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                int raw = key[i] - 'a';
+                for (int j = 0; j < 26; j++)
+                {
+                    if (tabulaRecta[raw, j] == cipherText[i])
+                    {
+                        plainText += (char)('a' + j);
+                        if (cipherText.Length != key.Length)
+                            key += key[i % key.Length];
+
+                        break;
+                    }
+                }
+            }
+            return plainText;
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            char[,] tabulaRecta = generateTabulaRecta();
+            string cipher = "";
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                cipher += tabulaRecta[plainText[i] - 'a', key[i % key.Length] - 'a'];
+            }
+            return cipher;
         }
+
+        private char[,] generateTabulaRecta()
+        {
+            char[,] tabulaRecta = new char[26, 26];
+
+            for (int i = 0; i < 26; i++)
+            {
+                char firstChar = (char)('A' + i);
+                for (int j = 0; j < 26; j++)
+                {
+                    if (firstChar + j > 'Z')
+                    {
+                        tabulaRecta[i, j] = (char)(((firstChar + j) % ('Z' + 1)) + 'A');
+                    }
+                    else
+                    {
+                        tabulaRecta[i, j] = (char)(firstChar + j);
+                    }
+                }
+            }
+
+            return tabulaRecta;
+        }
+
     }
 }
