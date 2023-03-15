689a35bc8c9b96933d86d55d0709d7dc10cfbb68
khalid-debugg <khaled20191700225@cis.asu.edu.eg>
2023-03-15 15:31:03 +0200

    programming is fun :'(

--- SecurityPackage/securitylibrary/MainAlgorithms/Columnar.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/Columnar.cs
@@ -11,53 +11,67 @@ namespace SecurityLibrary
 
         public List<int> Analyse(string plainText, string cipherText)
         {
-            /* plainText = plainText.ToLower();
-             cipherText = cipherText.ToLower();
-             int size = plainText.Length;
-             List<List<int>> Permute(int n)
-             {
-                 var result = new List<List<int>>();
-                 var nums = new List<int>();
-                 for (int i = 1; i <= n; i++) nums.Add(i);
-                 PermuteHelper(nums, 0, result);
-                 return result;
-             }
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            int size = plainText.Length;
 
-             void PermuteHelper(List<int> nums, int start, List<List<int>> result)
-             {
-                 if (start == nums.Count)
-                 {
-                     result.Add(new List<int>(nums));
-                     return;
-                 }
-                 for (int i = start; i < nums.Count; i++)
-                 {
-                     Swap(nums, start, i);
-                     PermuteHelper(nums, start + 1, result);
-                     Swap(nums, start, i);
-                 }
-             }
 
-             void Swap(List<int> nums, int i, int j)
-             {
-                 int temp = nums[i];
-                 nums[i] = nums[j];
-                 nums[j] = temp;
-             }
-             List<List<int>> permutation = new List<List<int>>();
-             string encrypted;
-             for (int i = 1; i <= size; i++) {
-                 permutation = Permute(i);
+            List<List<int>> Permute(int n)
+            {
+                List<List<int>> permutations = new List<List<int>>();
 
-                 for (int j = 0; j < permutation.Count; i++) {
-                     encrypted = Encrypt(plainText, permutation[j]);
-                     if (encrypted == cipherText) return permutation[j];
-                 }
-             }
+                List<int> nums = new List<int>();
+                for (int i = 1; i <= n; i++)
+                {
+                    nums.Add(i);
+                }
 
-             return null;
-            */
-            throw new NotImplementedException();
+                GeneratePermutations(permutations, nums, 0, n - 1);
+
+                return permutations;
+            }
+
+            void GeneratePermutations(List<List<int>> permutations, List<int> nums, int left, int right)
+            {
+                if (left == right)
+                {
+                    permutations.Add(new List<int>(nums));
+                }
+                else
+                {
+                    for (int i = left; i <= right; i++)
+                    {
+                        // Swap the current element with the first element
+                        // and recursively generate permutations with the remaining elements
+                        Swap(nums, left, i);
+                        GeneratePermutations(permutations, nums, left + 1, right);
+                        Swap(nums, left, i);
+                    }
+                }
+            }
+
+            void Swap(List<int> list, int i, int j)
+            {
+                int temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+
+
+            string encrypted;
+            for (int i = 1; i <= size; i++)
+            {
+                List<List<int>> permutation = new List<List<int>>();
+                permutation = Permute(i);
+
+                for (int j = 0; j < permutation.Count; j++)
+                {
+                    encrypted = Encrypt(plainText, permutation[j]);
+                    if (encrypted == cipherText) return permutation[j];
+                }
+            }
+
+            return null;
 
         }
 
