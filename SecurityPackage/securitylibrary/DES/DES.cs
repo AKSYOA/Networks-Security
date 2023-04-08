54e7230a456a175262e757505511d8ec332a633b
sa3d <saadwaleed2019@gmail.com>
2023-04-08 15:01:47 +0200

    DES is finished

--- SecurityPackage/securitylibrary/DES/DES.cs
+++ SecurityPackage/securitylibrary/DES/DES.cs
@@ -17,6 +17,51 @@ namespace SecurityLibrary.DES
         bool[] binaryKey48 = new bool[48];
         bool[] nxtLPlain32 = new bool[32];
 
+
+
+        int[,,] S_BoxArr = { { {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 },
+                          { 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 },
+                          { 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 },
+                          { 15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 } },
+
+                        { {15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10 },
+                          { 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5 },
+                          { 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15 },
+                          { 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 } },
+
+                        { {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
+                          { 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
+                          { 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
+                          { 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 } },
+
+                        { {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15 },
+                          { 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
+                          {10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
+                          {3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14} },
+
+                        { {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 },
+                          { 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 },
+                          { 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 },
+                          { 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 } },
+
+                        { {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11 },
+                          { 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8 },
+                          {9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
+                          {4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13} },
+
+                        { {4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1 },
+                          { 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 },
+                          { 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 },
+                          { 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 } },
+
+                        { {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 },
+                          { 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 },
+                          {7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8},
+                          {2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11} }
+        };
+
+
+
         static int[] expantionPermutation = {32,1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9,
                                              8 ,9 ,10,11,12,13,12,13,14,15,16,17,
                                              16,17,18,19,20,21,20,21,22,23,24,25,
@@ -54,6 +99,11 @@ namespace SecurityLibrary.DES
                 binary += ToBinary(temp[i].ToString());
             }
             return binary;
+        }
+
+        public static string ToDecimal(string val)
+        {
+            return Convert.ToString(Convert.ToInt32(val, 2), 10);
         }
 
         public string intialpermutation(string binary)
@@ -140,7 +190,7 @@ namespace SecurityLibrary.DES
             return result;
         }
 
-        public void setBinaryPlain(string plain)
+        public void setBinaryPlainRL(string plain)
         {
             for (int i = 0; i < 32; i++)
             {
@@ -173,7 +223,30 @@ namespace SecurityLibrary.DES
 
         public void SBox()
         {
+            for (int i = 0; i < 8; i++)
+            {
+                int s_bit = i * 6;
+                int e_bit = (i + 1) * 6 - 1;
 
+                string r1 = curRPlain48[s_bit] ? "1" : "0";
+                string r2 = curRPlain48[e_bit] ? "1" : "0";
+                string row = r1 + r2;
+
+                string column = "";
+                string res = "";
+
+                for (int j = s_bit + 1; j < e_bit; j++)
+                    column += curRPlain48[j] ? "1" : "0"; 
+
+                int r = Convert.ToInt32(ToDecimal(row));
+                int c = Convert.ToInt32(ToDecimal(column));
+
+                res = Convert.ToString(S_BoxArr[i, r, c], 2);
+                res = new string('0', 4 - res.Length) + res;
+
+                for (int j = 0; j < 4; j++) 
+                    curRPlain32[i * 4 + j] = res[j].Equals('1') ? true : false;
+            }
         }
 
 
@@ -210,11 +283,81 @@ namespace SecurityLibrary.DES
             return temp;
         }
 
+        public string Concatinate()
+        {
+            string cipher = "";
+            for (int i = 0; i < 32; i++)
+            {
+                cipher += curRPlain32[i] ? "1" : "0";
+            }
+            for (int i = 0; i < 32; i++)
+            {
+                cipher += curLPlain32[i] ? "1" : "0";
+            }
+            return cipher;
+        }
+
+        public string Final(string str)
+        {
+            string final = "";
+            for (int i = 0; i < str.Length; i += 4)
+            {
+                final += Tohexa(str.Substring(i, 4));
+            }
+            final = final.ToUpper();
+            final = final.Insert(0, "0x");
+            return final;
+        }
+
+
 
         public override string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            string plainbinary = "";
+            string keybinary = "";
+            string plain;
+
+            string[] keysOfRounds = new string[16];
+            cipherText = cipherText.Remove(0, 2);
+            key = key.Remove(0, 2);
+
+            keybinary = createbinary(key);
+            plainbinary = intialpermutation(createbinary(cipherText));
+            string[] C_D = permutedOne(keybinary);
+
+            for (int round = 1; round <= 16; round++)
+            {
+                C_D = LeftCircularShift(C_D, round);
+                key = C_D[0];
+                key += C_D[1];
+                key = permutedTwo(key);
+                keysOfRounds[round-1] = key;
+            }
+
+            // Ruond One.....
+            setBinaryPlainRL(plainbinary);
+
+            for (int round = 15; round >= 0; round--)
+            {
+                setBinarykey(keysOfRounds[round]);
+                Array.Copy(curRPlain32, nxtLPlain32, 32);
+                setExpand48();
+                XOR(ref curRPlain48, ref binaryKey48);
+                SBox();
+                PermutationAfterSBox();
+                XOR(ref curRPlain32, ref curLPlain32);
+                Array.Copy(nxtLPlain32, curLPlain32, 32);
+            }
+
+            //plain = Concatinate();
+            plain = InversPermutation(Concatinate());
+            //Console.WriteLine(plain);
+
+            return Final(plain);
         }
+
+
+
 
         public override string Encrypt(string plainText, string key)
         {
@@ -232,11 +375,11 @@ namespace SecurityLibrary.DES
             //C_D = permutedOne(keybinary);
 
             // Ruond One.....
-            setBinaryPlain(plainbinary);
+            setBinaryPlainRL(plainbinary);
 
-            for (int i = 1; i <= 16; i++)
+            for (int round = 1; round <= 16; round++)
             {
-                C_D = LeftCircularShift(C_D, i);
+                C_D = LeftCircularShift(C_D, round);
 
                 key = C_D[0]; key += C_D[1];
                 key = permutedTwo(key);
@@ -251,8 +394,12 @@ namespace SecurityLibrary.DES
                 Array.Copy(nxtLPlain32, curLPlain32, 32);
             }
 
+            //cipher = ConcatinateCipher();
+            cipher = InversPermutation (Concatinate());
+            //Console.WriteLine(cipher);
 
-            return "";
+            return Final(cipher);
+
         }
     }
 }
