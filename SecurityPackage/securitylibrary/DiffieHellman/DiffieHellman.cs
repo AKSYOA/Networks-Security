c12f1d9ed4ca0ce517619ca3e2ffe523556fbd94
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2023-04-23 15:18:19 +0200

    Implement DiffieHellman Algorithm

--- SecurityPackage/securitylibrary/DiffieHellman/DiffieHellman.cs
+++ SecurityPackage/securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -10,7 +10,25 @@ namespace SecurityLibrary.DiffieHellman
     {
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            int ya = FastPower(alpha, xa, q);
+            int yb = FastPower(alpha, xb, q);
+
+            int KA = FastPower(yb, xa, q);
+            int KB = FastPower(ya, xb, q);
+
+            return new List<int> { KA, KB };
+        }
+
+        private int FastPower(int M, int e, int n)
+        {
+            int res = 1;
+
+            for (int i = 0; i < e; i++)
+                res = (res * M) % n;
+
+            return res % n;
         }
     }
+
+
 }
